ff2673cee1af483f11404affad2d17d65a5dab8c
phamthanhnhanussh@gmail.com <phamthanhnhanussh@gmail.com>
2026-03-06 02:07:27 +0700

    Break the code by returning 500 as stat

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello CI/CD!" });
+  res.status(500).json({ message: "Hello CI/CD!" });
 });
 
 module.exports = app;
